ece71e63512e97b22000348d9d32d30b018c5e0c
YZhaozero <2293086836@qq.com>
2026-09-18 10:35:37 +0800

    add point2scan

---
+++ src/gs_nav_sensor_proc/pointcloud_to_laserscan/launch/pointcloud_to_laserscan_launch.py
@@ -0,0 +1,41 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            name='scanner', default_value='scanner',
+            description='Namespace for sample topics'
+        ),
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='true',
+            description='Use simulation (bag) clock if true'
+        ),
+
+        Node(
+            package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
+            remappings=[('cloud_in', '/livox/lidar/pointcloud'),
+                        ('scan', '/scan')],
+            parameters=[{
+                'target_frame': 'livox_frame',
+                'transform_tolerance': 0.01,
+                'min_height': -1.0,
+                'max_height': 0.1,
+                'angle_min': -3.14159,  # -M_PI/2
+                'angle_max': 3.14159,  # M_PI/2
+                'angle_increment': 0.0043,  # M_PI/360.0
+                'scan_time': 0.3333,
+                'range_min': 0.45,
+                'range_max': 10.0,
+                'use_inf': True,
+                'inf_epsilon': 1.0,
+                'use_sim_time': LaunchConfiguration('use_sim_time')
+            }],
+            name='pointcloud_to_laserscan',
+            output='screen'
+        )
+    ])
